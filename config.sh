04ee49cab680890142eaa5c6a4a6aa87ce4708ea
1209user <163229477+1209user@users.noreply.github.com>
2024-03-13 12:16:55 -0300

    config.sh

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="x689f" # codename used in device tree
+export DEVICE="X689F" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X689F" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
